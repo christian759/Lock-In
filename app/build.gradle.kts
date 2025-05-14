4c4443f11aee5cb572ffa8e26e191583152d2e81
christian759 <christian4onos@outlook.com>
2025-05-14 03:42:55 +0100

    build successful

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -37,7 +37,10 @@ android {
 }
 
 dependencies {
-
+    implementation(libs.datastore.preferences)
+    implementation(libs.material.icons.extended)
+    implementation(libs.navigation.compose)
+    implementation(libs.coil.compose)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
